b0087d4f3bbf95b3dd7eb3501efebb205c2c7d80
Celincia <23016929@myrp.edu.sg>
2024-07-31 10:16:02 +0800

    first commit

---
+++ RegistrationApp/app.js
@@ -0,0 +1,162 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+//******** TODO: Insert code to import 'express-session' *********//
+const session = require('express-session');
+
+const flash = require('connect-flash');
+
+const app = express();
+
+// Database connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'C237_L11_usersdb'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+//******** TODO: Insert code for Session Middleware below ********//
+app.use(session({
+    secret  : 'secret',
+    resave : false,
+    saveUninitialized : true,
+    //session expires after 1 week of inactivity
+    cookie: {maxAge:1000*60*60*24*7}
+}))
+
+app.use(flash());
+
+// Setting up EJS
+app.set('view engine', 'ejs');
+
+//******** TODO (ONLY IN L11B): Create a Middleware to check if user is logged in. ********//
+const checkAuthenticated = (req, res, next) => {
+    if (req.session.user) {
+        return next();
+    }else {
+        req.flash('error', 'Please log in to view this page');
+        res.redirect('/login');
+    }
+};
+
+//******** TODO (ONLY IN L11B): Create a Middleware to check if user is admin. ********//
+const checkAdmin = (req, res, next) => {
+    if (req.session.user.role === 'admin') {
+        return next();
+    } else {
+        req.flash('error', 'Access Denied');
+        res.redirect('/dashboard');
+    }
+};
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { user: req.session.user, messages: req.flash('success')});
+});
+
+app.get('/register', (req, res) => {
+    res.render('register', { messages: req.flash('error'), formData: req.flash('formData')[0] });
+});
+
+
+//******** TODO: Create a middleware function validateRegistration ********//
+
+const validateRegistration = (req, res, next) => {
+    const { username, email, password, address, contact } = req.body;
+    
+    if (!username|| !email || !password || !address || !contact) {
+        return res.status(400).send('All fields are required'); 
+    }
+    if (password.length < 6) {
+        req.flash('error', 'Password must be at least 6 characters long');
+        req.flash('formData', req.body);
+        return res.redirect('/register');
+    }
+    next();//if validation pass, the next function is called, allowing the request to proceed to the 
+    //next middleware or route handler 
+};
+
+
+//******** TODO: Integrate validateRegistration into the register route. ********//
+app.post('/register', validateRegistration, (req, res) => {
+
+    const { username, email, password, address, contact, role } = req.body;
+
+    //******** TODO (ONLY IN L11B): Update register route to include role. ********//
+
+    const sql = 'INSERT INTO users (username, email, password, address, contact, role) VALUES (?, ?, SHA1(?), ?, ?)';
+    db.query(sql, [username, email, password, address, contact, role], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+        req.flash('success', 'Registration successful! Please log in.');
+        res.redirect('/login');
+    });
+});
+
+//******** TODO: Insert code for login routes to render login page below ********//
+app.get('/login', (req, res) => {
+    res.render('login', { 
+        messages: req.flash('success'),
+        errors: req.flash('error')
+    });
+});
+
+//******** TODO: Insert code for login routes for form submission below ********//
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    
+    //validate email and password
+    if (!email || !password) {
+        req.flash('error', 'All fields are required.');
+        return res.redirect('/login');
+    }
+
+    const sql= 'SELECT * FROM users WHERE email = ? AND password = SHA1(?)';
+    db.query(sql,[email,password], (err, results) => {
+        if (err) {
+            throw err;
+        }
+
+        if (results.length > 0) {
+            //successful login
+            req.session.user = results[0];
+            req.flash('success', 'Login successful');
+            //redirect to dashboard todo(11b):update to redirect user to dasboard rout upon successful log in
+            res.redirect('/dashboard');
+        } else {
+            //invalid credentials
+            req.flash('error', 'Invalid email or password');
+            res.redirect('/login');
+        }
+    }
+)});
+//******** TODO (ONLY IN L11B): Insert code for dashboard route to render dashboard page for users. ********//
+app.get('/dashboard', checkAuthenticated, (req, res) => {
+    res.render('dashboard', { user: req.session.user });
+});
+//******** TODO (ONLY IN L11B): Insert code for admin route to render dashboard page for admin. ********//
+app.get('/admin', checkAuthenticated, checkAdmin, (req, res) => {
+    res.render('admin', { user: req.session.user });
+});
+//******** TODO: Insert code for logout route ********//
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+// Starting the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});// link to local host
